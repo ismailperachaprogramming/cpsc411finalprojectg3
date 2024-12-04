d420c0a58a61235351562fd1a40e1ceef067fc35
jnazzario <julinazzario@gmail.com>
2024-12-03 16:08:54 -0800

    Initial Commit

---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by Juli Nazzario on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
